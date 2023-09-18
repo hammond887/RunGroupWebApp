f543f4e587051da976d647c382007646d5ecf91e
Jonathan Hammond <jonathan.hammond@isitetv.com>
2023-09-18 17:24:55 +0100

    Dashboard-User-edit-and-GeoLocation

--- RunGroupWebApp/Repository/DashboardRepository.cs
+++ RunGroupWebApp/Repository/DashboardRepository.cs
@@ -1,4 +1,5 @@
-﻿using RunGroupWebApp.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroupWebApp.Data;
 using RunGroupWebApp.Interfaces;
 using RunGroupWebApp.Models;
 
@@ -27,6 +28,28 @@ namespace RunGroupWebApp.Repository
             var curUser = _contextAccessor.HttpContext?.User.GetUserId();
             var userRaces = _context.Races.Where(r => r.AppUser.Id == curUser.ToString());
             return userRaces.ToList();
+        }
+
+        public async Task<AppUser> GetByIdNoTracking(string id)
+        {
+            return await _context.Users.Where(u=> u.Id == id).AsNoTracking().FirstOrDefaultAsync();
+        }
+
+        public async Task<AppUser> GetUserById(string id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public bool Update(AppUser user)
+        {
+            _context.Users.Update(user);
+            return Save();
+        }
+
+        public bool Save()
+        { 
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
         }
     }
 }
